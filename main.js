e09561f9a0a3573aa37cf3dfd059d4a5c74b95f5
MLYAT <101877951+MLYAT@users.noreply.github.com>
2023-07-16 15:36:40 +0300

    add bold text and scroll

---
+++ main.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', function() {
+    var scrollButton = document.getElementById('scroll-to-top');
+    var mainBlock = document.querySelector('.main-block-left');
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+    if (scrollPosition > mainBlock.offsetHeight) {
+      scrollButton.classList.add('show');
+    } else {
+      scrollButton.classList.remove('show');
+    }
+  });
+  
+  document.getElementById('scroll-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+  
